bc906201f11f2d56a175d182310d6ce849549d32
adrielj <adrieljuarez2019@gmail.com>
2020-02-13 20:49:01 -0500

    improved version 1

--- src/GPA.java
+++ src/GPA.java
@@ -7,10 +7,10 @@ public class GPA {
 		Scanner in = new Scanner(System.in);
 		
 		String letterGrade;
-		double numberGrade;
-		boolean loop;
+		double numberGrade = 0;
+		Boolean loop = true;
 		
-		while (loop = true) {
+		while (loop == true) {
 		
 		System.out.println("To quit program, type \"Q\".");
 		System.out.print("What letter grade did you get?: ");
@@ -20,63 +20,53 @@ public class GPA {
 		switch (letterGrade) {
 		
 		case "A+": numberGrade = 4;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "A": numberGrade = 4;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "A-": numberGrade = 3.7;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "B+": numberGrade = 3.3;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "B": numberGrade = 3;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "B-": numberGrade = 2.7;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "C+": numberGrade = 2.3;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "C": numberGrade = 2;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "C-": numberGrade = 1.7;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "D+": numberGrade = 1.3;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "D": numberGrade = 1;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "D-": numberGrade = 0.7;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "F": numberGrade = 0;
-		System.out.println("Your number grade is a " +numberGrade);
 		break;
 		
 		case "Q": System.out.println("Thank you, goodbye.");
 		System.exit(0);
+		break;
 		
 		default: System.out.println("That isn't an acceptable answer.");
 		
 			}
+		
+		System.out.println("Your number grade is a " +numberGrade);
 		
 		}
 		
